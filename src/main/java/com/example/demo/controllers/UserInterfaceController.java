14844e23c4fe62d72c481877bf82931ac8c8d86f
Tapan Kumar <tapankumar.it@gmail.com>
2020-09-02 14:23:12 +0530

    nitial commit

---
+++ src/main/java/com/example/demo/controllers/UserInterfaceController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("demo")
+public class UserInterfaceController {
+
+    @Autowired @Qualifier("restTemplateBean")
+    RestTemplate rt;
+
+    @Autowired @Qualifier("str")
+    String str;
+
+    @RequestMapping(value="/")
+    public String hello() {
+//        return "Hello World"+rt.toString();
+        return "Hello World "+str;
+    }
+
+    @RequestMapping(value="/2nd")
+    public String hello2() {
+        return "Hello World 2";
+    }
+}
---
+++ src/main/java/com/example/demo/services/BeansDef.java
@@ -0,0 +1,27 @@
+package com.example.demo.services;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class BeansDef {
+
+    @Bean
+    @Qualifier("restTemplateBean")
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    @Qualifier("str")
+    public String str1(){
+        return "string qua";
+    }
+
+    @Bean
+    public String str2(){
+        return "string";
+    }
+}
